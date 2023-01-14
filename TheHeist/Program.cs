383548721c7da5e7ff3d8c44a5a9d6ed19c224c4
Ganesh Babu <ganeshbabudev65@gmail.com>
2023-01-14 12:39:24 -0500

    Add project files.

---
+++ TheHeist/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace TheHeist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Phase1
+            Console.WriteLine("Plan Your Heist!");
+
+            var teamMember = new TeamMember();
+
+            bool isNameValid;
+            string? name;
+
+            do
+            {
+                Console.WriteLine("Team Member Name:");
+                name = Console.ReadLine();
+
+                isNameValid = !string.IsNullOrWhiteSpace(name);
+
+
+                if (isNameValid) teamMember.Name = name;
+                else Console.WriteLine("Please enter a valid name!");
+
+
+            } while (!isNameValid);
+
+
+            bool isValidSkillLevel;
+            byte skillLevel;
+
+            do
+            {
+                Console.WriteLine("Skill Level:");
+                isValidSkillLevel = Byte.TryParse(Console.ReadLine(), out skillLevel);
+
+                if (isValidSkillLevel) teamMember.SkillLevel = skillLevel;
+                else Console.WriteLine("Skill Level should be a positive integer!");
+
+
+            } while (!isValidSkillLevel);
+
+
+            bool isValidCourageFactor;
+            double courageFactor;
+
+            do
+            {
+                Console.WriteLine("Courage Factor:");
+                isValidCourageFactor = Double.TryParse(Console.ReadLine(), out courageFactor);
+
+                if (isValidCourageFactor && courageFactor >= 0.0 && courageFactor <= 2.0) teamMember.CourageFactor = courageFactor;
+                else
+                {
+                    isValidCourageFactor = false;
+                    Console.WriteLine("Courage Factor should be between 0.0 and 2.0!");
+                }
+
+
+            } while (!isValidCourageFactor);
+
+            Console.WriteLine(teamMember.ToString());
+
+            #endregion
+
+        }
+
+    }
+}
---
+++ TheHeist/TeamMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHeist
+{
+    internal class TeamMember
+    {
+        public string? Name { get; set; }
+        public byte SkillLevel { get; set; }
+
+        public double CourageFactor { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} has skill level {this.SkillLevel} with courage factor {this.CourageFactor}";
+        }
+
+    }
+}
